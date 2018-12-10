c5fad13acec408dd2a9e6d84d21141bfd15428c6
Micah Young <myoung@pivotal.io>
2018-12-10 15:35:44 -0500

    Initial implementation

--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using Steeltoe.Common.Net;
 
 namespace SMBDemo
 {
@@ -10,6 +13,30 @@ namespace SMBDemo
     {
         static void Main(string[] args)
         {
+            string uncPath = args[0];
+            string username = args[1];
+            string password = args[2];
+            string domain = ""; // domain not used
+
+            NetworkCredential credential = new NetworkCredential(username, password, domain);
+            using (WindowsNetworkFileShare networkPath = new WindowsNetworkFileShare(uncPath, credential))
+            {
+                FindFiles(uncPath);
+            }
+
         }
+
+        public static void FindFiles(string targetDirectory)
+        {
+            string[] fileEntries = Directory.GetFiles(targetDirectory);
+            foreach (string fileName in fileEntries)
+                Console.WriteLine("Found file '{0}'.", fileName);
+
+            string[] subdirectoryEntries = Directory.GetDirectories(targetDirectory);
+            foreach (string subdirectory in subdirectoryEntries)
+                FindFiles(subdirectory);
+        }
+
+
     }
 }
